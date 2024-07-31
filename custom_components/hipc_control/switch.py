cbdcb2ee0c0400ede0373e7b12861d3667bf1504
Seele Vol <48525679+Seele0oO@users.noreply.github.com>
2024-07-31 12:04:55 +0800

    Update configuration to register switch as an entity

--- custom_components/hipc_control/switch.py
+++ custom_components/hipc_control/switch.py
@@ -3,17 +3,36 @@ import json
 import logging
 
 from homeassistant.components.switch import SwitchEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.service import verify_domain_control
+import voluptuous as vol
+
 from .const import DOMAIN, CONF_PHONE, CONF_USER_KEY, CONF_MAC
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
-    data = hass.data[DOMAIN]
-    phone = data[CONF_PHONE]
-    user_key = data[CONF_USER_KEY]
-    mac = data[CONF_MAC]
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
+    phone = entry.data[CONF_PHONE]
+    user_key = entry.data[CONF_USER_KEY]
+    mac = entry.data[CONF_MAC]
 
-    add_entities([HiPCSwitch(phone, user_key, mac)])
+    switch = HiPCSwitch(phone, user_key, mac)
+    async_add_entities([switch])
+
+    async def handle_restart_service(call):
+        entity_id = call.data.get("entity_id")
+        target_switch = next((s for s in hass.data[DOMAIN].values() if s.entity_id == entity_id), None)
+        if target_switch:
+            target_switch.restart()
+
+    hass.services.async_register(
+        DOMAIN, "restart_pc", handle_restart_service,
+        schema=vol.Schema({
+            vol.Required("entity_id"): cv.entity_id,
+        })
+    )
 
 class HiPCSwitch(SwitchEntity):
     def __init__(self, phone, user_key, mac):
@@ -33,10 +52,12 @@ class HiPCSwitch(SwitchEntity):
     def turn_on(self, **kwargs):
         self._send_request("1")
         self._state = True
+        self.schedule_update_ha_state()
 
     def turn_off(self, **kwargs):
         self._send_request("0")
         self._state = False
+        self.schedule_update_ha_state()
 
     def restart(self, **kwargs):
         self._send_request("2")
